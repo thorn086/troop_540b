c4dce271a5525f022878b9cc319d166ba9c1c4cb
Darren_Rose <thorn086@gmail.com>
2021-05-26 18:07:39 -0500

    added resources

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Route } from 'react-router-dom';
 import  Home  from './components/Home/Home'
 import About from './components/About/About'
 import Contacts from './components/Contacts/Contacts'
+import Resources from './components/Resources/Resources'
+
+
 class App extends Component {
 
   render() {
@@ -10,7 +13,8 @@ class App extends Component {
       <div>
         <Route  exact path='/' component={Home} />
         <Route  path='/About' component={About} />
-        <Route  path='/Contacts' component={Contacts} />   
+        <Route  path='/Contacts' component={Contacts} />
+        <Route  path='/Resources' component={Resources} />   
       </div>
     );
   }
---
+++ src/components/Resources/Resources.js
@@ -0,0 +1,39 @@
+import "./Resources.css";
+import NavBar from "../NavBar/NavBar";
+import React, { Component } from "react";
+
+class Resources extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header id="Res-header">
+          <h1>Resources Page </h1>
+        </header>
+        <nav>
+          <NavBar />
+        </nav>
+        <section id="Res_Info">
+
+          <div id="Res_box">
+            <section id='troop_res'>
+              <h2 >Resources for Troop 540</h2>
+              <h3 id='Res_h3'>2021 Updates</h3>
+              <a target="blank" href='https://www.scout.org' >Scouts</a>
+            </section>
+            <section id='bsa_res'>
+              <h2 >Scouting Resources</h2>
+              <h3 id='Res_h3'>2021 Updates</h3>
+              <a target="blank" href='https://www.scout.org' >Scouts</a>
+            </section>
+          </div>
+        </section>
+        <footer className="ft_page_main">
+          <p>Copy Right 2021</p>
+          <p> Web_Master J.R.</p>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default Resources;
