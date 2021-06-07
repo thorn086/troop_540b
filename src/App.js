dc3ad41e7a6fa991d1e4726432f9f630a140b533
darkknight <darkknight23477@gmail.com>
2021-06-07 18:35:21 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import { Route } from 'react-router-dom';
-import Home  from './components/Home/Home'
-import About from './components/About/About'
-import Contacts from './components/Contacts/Contacts'
-import Resources from './components/Resources/Resources'
+import Home  from './components/Home/Home';
+import About from './components/About/About';
+import Contacts from './components/Contacts/Contacts';
+import Resources from './components/Resources/Resources';
+import Gallery from './components/Gallery/Gallery';
 
 
 class App extends Component {
@@ -14,7 +15,8 @@ class App extends Component {
         <Route  exact path='/' component={Home} />
         <Route  path='/About' component={About} />
         <Route  path='/Contacts' component={Contacts} />
-        <Route  path='/Resources' component={Resources} />   
+        <Route  path='/Resources' component={Resources} />
+        <Route  path='/Gallery' component={Gallery} />    
       </div>
     );
   }
--- src/components/Sidebar/SidebarData.js
+++ src/components/Sidebar/SidebarData.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as AiIcons from 'react-icons/ai';
 import * as IoIcons from 'react-icons/io';
+import * as FaIcons from 'react-icons/fa';
 
 export const SidebarData = [
   {
@@ -19,6 +20,12 @@ export const SidebarData = [
     title: 'Contacts',
     path: '/Contacts',
     icon: <IoIcons.IoMdPeople />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Gallery',
+    path: '/Gallery',
+    icon: <FaIcons.FaPhotoVideo />,
     cName: 'nav-text'
   },
   {
