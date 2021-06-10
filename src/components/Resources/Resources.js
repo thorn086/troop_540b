0b41d7ffd91dc17536b7e8f010d3b10d51213c46
Darren_Rose <thorn086@gmail.com>
2021-06-10 15:46:18 -0500

    added scoutbook link

--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -15,8 +15,8 @@ class Resources extends Component {
               <h2>Resources for Troop 540</h2>
               <h3 id="Res_h3">2021 Updates</h3>
               <div className="sites">
-                <a className='link_btn' target="blank" href="https://www.scout.org">
-                  Scouts
+                <a className='link_btn' target="blank" href="https://scoutbook.scouting.org/?_gl=1*ja4vgd*_ga*NDI4MTQyMTA1LjE2MjMzNTc5MDE.*_ga_20G0JHESG4*MTYyMzM1NzkwMC4xLjAuMTYyMzM1NzkwMC42MA..">
+                  Scout Book
                 </a>
                 <br />
                 <a className='link_btn' target="blank" href="https://www.threeharborsscouting.org">
