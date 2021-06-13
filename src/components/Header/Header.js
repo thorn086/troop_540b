ec82da2f6fa1d2a08b762da976c872fbf9566bbf
Darren_Rose <thorn086@gmail.com>
2021-06-13 09:31:13 -0500

    change header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,11 +8,8 @@ function Header() {
     return (
         <header className='header_title'>
            <Sidebar /> 
-            <div id='logo_box'>
                 <img id='bsa_logo' src={bssym} alt="boy scouts of america symbol" />
                 <h2 id='troop_tag'>Troop 540</h2>
-            </div>
-            
         </header>
     )
 }
