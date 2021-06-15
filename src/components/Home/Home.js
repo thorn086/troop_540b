1337e600265f8c614f89fa402da69a66682a9d37
darkknight <darkknight23477@gmail.com>
2021-06-15 18:20:33 -0500

    updtates

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,39 +14,7 @@ class Home extends Component {
                     <h2 className='home-info'>**** Future Home of Troop 540 ****</h2>
                     <h6 className='home-info'>Check Back Soon!</h6>
                 </section>
-                <section id='events'>
-                    <h2> Up Coming Events</h2>
-                    <div id="event_dates">
-                        <div id='special-dates'>
-
-                            <h5 className='dates'> Scouting For Food June 12th, 2021 8:30 PM <br></br><a id='st_charels' target="blank" href="https://www.google.com/maps/place/3100+W+Parnell+Ave,+Milwaukee,+WI+53221/@42.9427161,-87.9567283,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ac9697935:0xe92986f1420fc0d1!8m2!3d42.9427161!4d-87.9545396?hl=en">
-                                St. Charels Borromeo
-                            </a></h5>
-                            <br></br>
-
-                            <h5 className='dates'> Scouting For Food June 19th, 2021 9:00 PM <br></br><a id='st_charels' target="blank" href="https://www.google.com/maps/place/3100+W+Parnell+Ave,+Milwaukee,+WI+53221/@42.9427161,-87.9567283,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ac9697935:0xe92986f1420fc0d1!8m2!3d42.9427161!4d-87.9545396?hl=en">
-                                St. Charels Borromeo
-                            </a></h5>
-                            <br></br>
-                        </div>
-
-                        <div id='troop-dates'>
-
-                            <h5 className='dates'> Troop Meeting June 24th, 2021 6:30 - 8:30 PM <br></br><a id='st_charels' target="blank" href="https://www.google.com/maps/place/3100+W+Parnell+Ave,+Milwaukee,+WI+53221/@42.9427161,-87.9567283,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ac9697935:0xe92986f1420fc0d1!8m2!3d42.9427161!4d-87.9545396?hl=en">
-                                St. Charels Borromeo
-                            </a></h5>
-                            <br></br>
-
-                            <h5 className='dates'> Troop Meeting July 15th, 2021 6:30 - 8:30 PM <br></br><a id='st_charels' target="blank" href="https://www.google.com/maps/place/3100+W+Parnell+Ave,+Milwaukee,+WI+53221/@42.9427161,-87.9567283,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ac9697935:0xe92986f1420fc0d1!8m2!3d42.9427161!4d-87.9545396?hl=en">
-                                St. Charels Borromeo
-                            </a></h5>
-                            <br></br>
-                            <p>*** Scouts that participated in the exercise requirement from the June 10th meeting will complete the second half at the July 8th Troop Meeting **</p>
-                        </div>
-
-
-                    </div>
-                </section>
+               
                 <Footer />
             </div>
         );
