2f03665539aa89492e8f85c2352fa3d31eb0f8f8
darkknight <darkknight23477@gmail.com>
2021-06-15 20:20:33 -0500

    test

--- src/components/Important_Info/Info.js
+++ src/components/Important_Info/Info.js
@@ -11,7 +11,7 @@ function Info(){
         <p>Info text test</p>
         <br></br>
         <p>I am writing this paragraph to see if the information section will display the information we need.
-            Testing this beforehand makes sure that our website is as efficient as possible, which will help the rest of the troop. :)
+            Testing this beforehand makes sure that our website is as efficient as possible, which will help the rest of the troop.
         </p>
             </div>
             </section>
