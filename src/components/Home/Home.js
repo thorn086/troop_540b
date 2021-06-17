642ff6451524560edb7221ea7354452bb2e7ab67
Darren_Rose <thorn086@gmail.com>
2021-06-17 16:43:11 -0500

    updating modal info

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,28 +1,33 @@
 import './Home.css'
 import construction from '../../img/construction_img.jpeg';
-import React, { Component } from 'react';
+import React, {  useState } from 'react';
 import Footer from '../footer/footer';
 import Header from '../Header/Header';
 import Events from '../Events/Events';
 import Info from '../Important_Info/Info';
 
-class Home extends Component {
-    render() {
+function Home() {
+    
+    
+        const [buttonPopup, setButtonPopup]= useState(false);
         return (
-            <div className="App">
-                <Header />
-                <section id='Main_title'>
-                    <img id='const_logo' src={construction} alt="construction workers" />
-                    <h1 id='home-title'>Site Under Construction</h1>
-                    <h2 className='home-info'>**** Future Home of Troop 540 ****</h2>
-                    <h6 className='home-info'>Check Back Soon!</h6>
-                </section>
-                <Events />
-                <Info/>
-                <Footer />
-            </div>
+                <div className="App">
+                    <Header />
+                    <section id='Main_title'>
+                        <img id='const_logo' src={construction} alt="construction workers" />
+                        <h1 id='home-title'>Site Under Construction</h1>
+                        <h2 className='home-info'>**** Future Home of Troop 540 ****</h2>
+                        <h6 className='home-info'>Check Back Soon!</h6>
+                    </section>
+
+                    <Events />
+                    <Info trigger={buttonPopup} setTrigger={setButtonPopup}/>
+                    <button id ='button_info' className='button_info_one' onClick={()=> setButtonPopup(true)} >Important Info</button>
+
+                    <Footer />
+                </div>
         );
     }
-}
+
 
 export default Home;
--- src/components/Important_Info/Info.js
+++ src/components/Important_Info/Info.js
@@ -2,20 +2,25 @@ import React from "react";
 import './Info.css';
 
 
+//  */
 
-function Info(){
-    return(
-        <section id='info'>
-        <h2> Important Info</h2>
-        <div id="info_text">
-        <p>Info text test</p>
-        <br></br>
-        <p>I am writing this paragraph to see if the information section will display the information we need.
-            Testing this beforehand makes sure that our website is as efficient as possible, which will help the rest of the troop.
-        </p>
+
+function Info(props) {
+    return (props.trigger) ? (
+        <div className='info'>
+            <div className="info_content">
+                <button className="close" onClick={() => props.setTrigger(false)}>X</button>
+                <h2> Important Info</h2>
+                <div id="info_text">
+                    <p>Info text test</p>
+                    <br></br>
+                    <p>I am writing this paragraph to see if the information section will display the information we need.
+                    Testing this beforehand makes sure that our website is as efficient as possible, which will help the rest of the troop.
+</p>
+                </div>
             </div>
-            </section>
-    );
+        </div>
+    ) : "";
 
 }
 export default Info;
