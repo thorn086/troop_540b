b71f3c0f771f39b8b28297cbd57e212d91134227
darkknight <darkknight23477@gmail.com>
2021-06-17 18:13:44 -0500

    updated css for info modol

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -18,11 +18,12 @@ function Home() {
                         <h1 id='home-title'>Site Under Construction</h1>
                         <h2 className='home-info'>**** Future Home of Troop 540 ****</h2>
                         <h6 className='home-info'>Check Back Soon!</h6>
+                        <button id ='button_info' className='button_info_one' onClick={()=> setButtonPopup(true)} >Important Info</button>
                     </section>
 
                     <Events />
                     <Info trigger={buttonPopup} setTrigger={setButtonPopup}/>
-                    <button id ='button_info' className='button_info_one' onClick={()=> setButtonPopup(true)} >Important Info</button>
+                    
 
                     <Footer />
                 </div>
