3306652b3e1740ea862c38779c8b25e0497a9c14
Darren_Rose <thorn086@gmail.com>
2021-06-17 22:35:26 -0500

    update home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -18,7 +18,8 @@ function Home() {
                     <section id='Main_title'>
                         <img id='const_logo' src={construction} alt="construction workers" />
                         <h1 id='home-title'>Site Under Construction</h1>
-                        <h2 className='home-info'>**** Future Home of Troop 540 ****</h2>
+                        <h3 className='home-info'> Future Home of Troop 540 </h3>
+                        <h2 className='home-info'>**** Parents don't forget to click the "540 Info" button for important troop information! ****</h2>
                         <h6 className='home-info'>Check Back Soon!</h6>
                     </section>
                     <Events />   
