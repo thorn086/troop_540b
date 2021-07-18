d2c832c7b76c0561fb717f64ba29e545d4326376
darkknight <darkknight23477@gmail.com>
2021-07-18 12:56:44 -0500

    added footerf links

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
 
 function Footer(){
     return(
         <footer className="ft_page_main">
         <p>Copy Right 2021</p>
         <p> Web_Master J.R.</p>
+        <ul>
+        <li><a target="blank" href="https://scoutbook.scouting.org/?_gl=1*ja4vgd*_ga*NDI4MTQyMTA1LjE2MjMzNTc5MDE.*_ga_20G0JHESG4*MTYyMzM1NzkwMC4xLjAuMTYyMzM1NzkwMC42MA..">Scoutbook</a></li>
+        <li><a target="blank" href="https://www.scouting.org/">BSA</a></li>
+        <li><a target="blank" href="https://www.scoutshop.org/">BSA Store</a></li>
+        </ul>
+
+
       </footer>
+      
     );
+
 }
 export default Footer;
