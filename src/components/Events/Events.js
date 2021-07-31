8c1003b3a1315e42c5a0a7160a29eb99d74e4ed6
Darren_Rose <thorn086@gmail.com>
2021-07-31 09:31:59 -0500

    style changes

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -12,7 +12,9 @@ function Events(){
         <div id="event_dates">
             <div id='special-dates'>
 
-            <h5 className='dates'> Summer Camp  July 24th (departing: 8:30 - 9:00 AM)-31st (returning 3:00 PM), 2021  <br></br><a id='st_charels' target="blank" href="https://www.google.com/maps/place/3100+W+Parnell+Ave,+Milwaukee,+WI+53221/@42.9427161,-87.9567283,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ac9697935:0xe92986f1420fc0d1!8m2!3d42.9427161!4d-87.9545396?hl=en">
+            <h5 className='dates'> Summer Camp July 24th (departing: 9:00 AM) 
+                                    - 31st (returning 3:00 PM), 2021  <br></br>
+                                    <a id='st_charels' target="blank" href="https://www.google.com/maps/place/3100+W+Parnell+Ave,+Milwaukee,+WI+53221/@42.9427161,-87.9567283,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ac9697935:0xe92986f1420fc0d1!8m2!3d42.9427161!4d-87.9545396?hl=en">
                     St. Charels Borromeo
                 </a></h5>
                 <br></br>
