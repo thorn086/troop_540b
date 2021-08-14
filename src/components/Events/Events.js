7fe78dff27db932f7c00102b46fca2e9ec57f423
Darren_Rose <thorn086@gmail.com>
2021-08-14 09:44:26 -0500

    updated dates

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -12,17 +12,10 @@ function Events(){
         <div id="event_dates">
             <div id='special-dates'>
 
-            <h5 className='dates'> Summer Camp July 24th (departing: 9:00 AM) 
-                                    - 31st (returning 3:00 PM), 2021  <br></br>
+            <h5 className='dates'> Saturday, August 28th, 2021 - 9 PM to Sunday, August 29th, 2021 - 6:30 AM "Tent Grounds Gaurding"<br></br>
                                     <a id='st_charels' target="blank" href="https://www.google.com/maps/place/3100+W+Parnell+Ave,+Milwaukee,+WI+53221/@42.9427161,-87.9567283,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ac9697935:0xe92986f1420fc0d1!8m2!3d42.9427161!4d-87.9545396?hl=en">
                     St. Charels Borromeo
                 </a></h5>
-                <br></br>
-                
-                <h5 className='dates'> Summer Camp Family Day July 30th, 2021  <br></br><a id='camp_phillips' target="blank" href="https://campphillips.org/parents">
-                    Camp Phillips
-                </a></h5>
-                <br></br>
 
             </div>
         </div>
