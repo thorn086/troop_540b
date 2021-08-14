bad387122f0f0c9d3cdb0622980640a12e476515
Darren_Rose <thorn086@gmail.com>
2021-08-14 10:29:41 -0500

    spelling correction

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -12,7 +12,7 @@ function Events(){
         <div id="event_dates">
             <div id='special-dates'>
 
-            <h5 className='dates'> August 28th, 2021 - 9 PM to August 29th, 2021 - 6:30 AM "Tent Grounds Gaurding"<br></br>
+            <h5 className='dates'> August 28th, 2021 - 9 PM to August 29th, 2021 - 6:30 AM "Gaurding Tent Grounds"<br></br>
                                     <a id='st_charels' target="blank" href="https://www.google.com/maps/place/3100+W+Parnell+Ave,+Milwaukee,+WI+53221/@42.9427161,-87.9567283,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ac9697935:0xe92986f1420fc0d1!8m2!3d42.9427161!4d-87.9545396?hl=en">
                     St. Charels Borromeo
                 </a></h5>
