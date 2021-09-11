90856f0427e5e977b9db4484a77f1a41ccd6fdc0
Darren_Rose <thorn086@gmail.com>
2021-09-11 14:13:10 -0500

    updated dates

--- src/components/Events/TroopEvents.js
+++ src/components/Events/TroopEvents.js
@@ -5,19 +5,12 @@ import './Events.css';
 
 
 
-<<<<<<< HEAD
 function TroopMeet() {
     return (
         <section id='troop__events'>
             <h2> Up Coming Troop Meetings</h2>
             <div id="event_dates">
                 <div id='troop-dates'>
-                    <h5 className='dates'> Troop Meeting September 9th, 2021 6:30 - 8:30 PM
-                        <br />
-                        <a id='st_charels' target="blank" href="https://www.google.com/maps/place/3100+W+Parnell+Ave,+Milwaukee,+WI+53221/@42.9427161,-87.9567283,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ac9697935:0xe92986f1420fc0d1!8m2!3d42.9427161!4d-87.9545396?hl=en">
-                            St. Charels Borromeo
-                        </a></h5>
-                    <br />
                     <h5 className='dates'> Troop Meeting September 23rd, 2021 6:30 - 8:30 PM
                         <br />
                         <a id='st_charels' target="blank" href="https://www.google.com/maps/place/3100+W+Parnell+Ave,+Milwaukee,+WI+53221/@42.9427161,-87.9567283,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ac9697935:0xe92986f1420fc0d1!8m2!3d42.9427161!4d-87.9545396?hl=en">
@@ -25,53 +18,6 @@ function TroopMeet() {
                         </a></h5>
                     <br />
                 </div>
-||||||| merged common ancestors
-function TroopMeet(){
-    return(
-        <section id='troopevents'>
-        <h2> Up Coming Troop Meetings</h2>
-        <div id="event_dates">
-            <div id='troop-dates'>
-
-                
-
-                <h5 className='dates'> Troop Meeting September 9th, 2021 6:30 - 8:30 PM 
-                <br />
-                <a id='st_charels' target="blank" href="https://www.google.com/maps/place/3100+W+Parnell+Ave,+Milwaukee,+WI+53221/@42.9427161,-87.9567283,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ac9697935:0xe92986f1420fc0d1!8m2!3d42.9427161!4d-87.9545396?hl=en">
-                    St. Charels Borromeo
-                </a></h5>
-                <br />
-
-                <h5 className='dates'> Troop Meeting September 23rd, 2021 6:30 - 8:30 PM 
-                <br />
-                <a id='st_charels' target="blank" href="https://www.google.com/maps/place/3100+W+Parnell+Ave,+Milwaukee,+WI+53221/@42.9427161,-87.9567283,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ac9697935:0xe92986f1420fc0d1!8m2!3d42.9427161!4d-87.9545396?hl=en">
-                    St. Charels Borromeo
-                </a></h5>
-                <br />
-=======
-function TroopMeet(){
-    return(
-        <section id='troopevents'>
-        <h2> Up Coming Troop Meetings</h2>
-        <div id="event_dates">
-            <div id='troop-dates'>
-
-                
-
-                <h5 className='dates'> Troop Meeting September 23rd, 2021 6:30 - 8:30 PM 
-                <br />
-                <a id='st_charels' target="blank" href="https://www.google.com/maps/place/3100+W+Parnell+Ave,+Milwaukee,+WI+53221/@42.9427161,-87.9567283,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ac9697935:0xe92986f1420fc0d1!8m2!3d42.9427161!4d-87.9545396?hl=en">
-                    St. Charels Borromeo
-                </a></h5>
-                <br />
-
-                <h5 className='dates'> Troop Meeting October 13th, 2021 6:30 - 8:30 PM 
-                <br />
-                <a id='st_charels' target="blank" href="https://www.google.com/maps/place/3100+W+Parnell+Ave,+Milwaukee,+WI+53221/@42.9427161,-87.9567283,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ac9697935:0xe92986f1420fc0d1!8m2!3d42.9427161!4d-87.9545396?hl=en">
-                    St. Charels Borromeo
-                </a></h5>
-                <br />
->>>>>>> b827642933e6e849da169b58382dfe760e099633
             </div>
         </section>
     );
