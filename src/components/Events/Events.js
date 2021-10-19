f07a13949ae5c2c6d5b1def082762a5efb70ed27
darkknight <darkknight23477@gmail.com>
2021-10-19 16:36:50 -0500

    updated october dates

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -11,7 +11,12 @@ function Events(){
         <h2> Up Coming Troop Outings</h2>
         <div id="event_dates">
             <div id='special-dates'>
-            <h5 className='dates'>No Upcoming Events </h5>
+
+            <h5 className='dates'> Court of Honor October 24th, 2021
+                        <br />
+                        <a id='st_charels' target="blank" href="https://www.google.com/maps/place/3100+W+Parnell+Ave,+Milwaukee,+WI+53221/@42.9427161,-87.9567283,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ac9697935:0xe92986f1420fc0d1!8m2!3d42.9427161!4d-87.9545396?hl=en">
+                            St. Charels Borromeo
+                        </a></h5>
 
             </div>
         </div>
