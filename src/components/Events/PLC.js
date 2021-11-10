d5dd5d89ca326bdcc394c451e07b3c113a591e30
darkknight <darkknight23477@gmail.com>
2021-11-09 18:35:00 -0600

    updated dates

--- src/components/Events/PLC.js
+++ src/components/Events/PLC.js
@@ -11,7 +11,7 @@ function PLC() {
             <h2> Up Coming PLC Meetings</h2>
             <div id="plc_dates">
                 <div id='troop-dates'>
-                        <h5 className='dates'> PLC Meeting November 8th, 2021 6:00 - 6:30 PM
+                        <h5 className='dates'> PLC Meeting December 13th, 2021 6:00 - 6:30 PM
                         <br />
                         <p>Mr. Pucel's House</p> </h5>
                     <br />
--- src/components/Events/TroopEvents.js
+++ src/components/Events/TroopEvents.js
@@ -12,13 +12,13 @@ function TroopMeet() {
             <div id="event_dates">
                 <div id='troop-dates'>
                     <h4>ALL scouts please BRING YOUR SCOUT BOOKS and WEAR CLASS A UNIFORM during troop meetings and outings!</h4>
-                        <h5 className='dates'> Troop Meeting October 28th, 2021 6:30 - 8:30 PM
+                        <h5 className='dates'> Troop Meeting November 11th, 2021 6:30 - 8:30 PM
                         <br />
                         <a id='st_charels' target="blank" href="https://www.google.com/maps/place/3100+W+Parnell+Ave,+Milwaukee,+WI+53221/@42.9427161,-87.9567283,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ac9697935:0xe92986f1420fc0d1!8m2!3d42.9427161!4d-87.9545396?hl=en">
                             St. Charels Borromeo
                         </a></h5>
                         <br />
-                        <h5 className='dates'> Troop Meeting November 11th, 2021 6:30 - 8:30 PM
+                        <h5 className='dates'> Troop Meeting December 9th, 2021 6:30 - 8:30 PM
                         <br />
                         <a id='st_charels' target="blank" href="https://www.google.com/maps/place/3100+W+Parnell+Ave,+Milwaukee,+WI+53221/@42.9427161,-87.9567283,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ac9697935:0xe92986f1420fc0d1!8m2!3d42.9427161!4d-87.9545396?hl=en">
                             St. Charels Borromeo
