488c8f2a87b40e8a73e7ed22716b30846f2fa9fa
darkknight <darkknight23477@gmail.com>
2022-01-25 18:42:32 -0600

    updated resourse page

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -11,12 +11,7 @@ function Events(){
         <h2> Troop Outings</h2>
         <div id="event_dates">
             <div id='special-dates'>
-            <h5 className='dates'> Klondike January 21-23rd, 2022
-                        <br />
-                        <a id='st_charels' target="blank" href="https://www.threeharborsscouting.org/camping/indian-mound-scout-reservation/61340">
-                            Indian Mound Scout Reservation
-                        </a></h5>
-                    <br />
+            <p>No upcoming outings at this time.</p>
             </div>
         </div>
     </section>
