2dfe2ce000e9a979c127fafa5cbf3a3c8607b9b1
darkknight <darkknight23477@gmail.com>
2022-02-28 17:46:43 -0600

    updated dates

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -11,11 +11,7 @@ function Events(){
         <h2> Troop Outings</h2>
         <div id="event_dates">
             <div id='special-dates'>
-            <h5 className='dates'> Hiking/Skiing Febuary 26th, 2022
-                        <br />
-                        <a id='st_charels' target="blank" href="https://www.visitmilwaukee.org/partners/whitnall-park-3060/">
-                            Whitnall Park
-                        </a></h5>
+            <p>No upcoming troop outings at this time.</p>
             </div>
         </div>
     </section>
