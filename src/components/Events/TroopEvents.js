46594ba9892efe084dfec4b6eac9629af3634f04
darkknight <darkknight23477@gmail.com>
2022-03-27 15:34:35 -0500

    updated dates

--- src/components/Events/TroopEvents.js
+++ src/components/Events/TroopEvents.js
@@ -12,12 +12,6 @@ function TroopMeet() {
             <div id="event_dates">
                 <div id='troop-dates'>
                     <h4>ALL scouts please BRING YOUR SCOUT BOOKS and WEAR CLASS A UNIFORM during troop meetings and outings!</h4>
-                    <h5 className='dates'> Troop Meeting March 24th, 2022 6:30 - 8:30 PM ** Kringle Sale orders and money due/summer camp money**
-                        <br />
-                        <a  id='st_charels' target="blank" href="https://www.google.com/maps/place/3100+W+Parnell+Ave,+Milwaukee,+WI+53221/@42.9427161,-87.9567283,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ac9697935:0xe92986f1420fc0d1!8m2!3d42.9427161!4d-87.9545396?hl=en">
-                            St. Charels Borromeo
-                        </a></h5>
-                    <br />
                     <h5 className='dates'> Troop Meeting April 14th, 2022 6:30 - 8:30 PM
                         <br />
                         <a id='st_charels' target="blank" href="https://www.google.com/maps/place/3100+W+Parnell+Ave,+Milwaukee,+WI+53221/@42.9427161,-87.9567283,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ac9697935:0xe92986f1420fc0d1!8m2!3d42.9427161!4d-87.9545396?hl=en">
