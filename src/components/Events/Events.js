563857f80786a2f328c4fa7511b7160e6c2017aa
Darren_Rose <thorn086@gmail.com>
2022-06-10 09:53:02 -0500

    updated dates

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -5,16 +5,16 @@ import './Events.css';
 
 
 
-function Events(){
-    return(
+function Events() {
+    return (
         <section id='events'>
-        <h2> Troop Outings</h2>
-        <div id="event_dates">
-            <div id='special-dates'>
-            <p>No upcoming troop outings at this time.</p>
+            <h2> Troop Outings</h2>
+            <div id="event_dates">
+                <div id='special-dates'>
+                    <p>Will's Eagle Scout project June 12th at 9am.</p>
+                </div>
             </div>
-        </div>
-    </section>
+        </section>
     );
 }
 export default Events;
