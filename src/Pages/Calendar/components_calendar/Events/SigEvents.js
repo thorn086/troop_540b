4ce6c24476a2f95384a446e87b587a34a55b6de5
darkknight <darkknight23477@gmail.com>
2022-06-26 15:31:46 -0500

    updated dates

--- src/Pages/Calendar/components_calendar/Events/SigEvents.js
+++ src/Pages/Calendar/components_calendar/Events/SigEvents.js
@@ -13,7 +13,7 @@ function SigEvent() {
                 <div id='significant-dates'>
                         <h5 className='dates'> Summer Camp July 23-30th, 2022
                         <br />
-                        <a id='st_charels' target="blank" href="https://campphillips.org">
+                        <a id='st_charels' target="blank" href="https://www.google.com/maps/place/Camp+Phillips/@45.6158573,-91.8284635,17z/data=!4m5!3m4!1s0x52ac545495f14d87:0x839ff6dc0b1362b8!8m2!3d45.6158536!4d-91.8262695">
                             Camp Philips
                         </a></h5>
                 </div>
