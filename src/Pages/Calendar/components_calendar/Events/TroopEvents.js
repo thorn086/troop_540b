26792b8e36eed391f0628396aea35d0340d5c0bd
darkknight <darkknight23477@gmail.com>
2022-06-26 15:21:59 -0500

    updated dates

--- src/Pages/Calendar/components_calendar/Events/TroopEvents.js
+++ src/Pages/Calendar/components_calendar/Events/TroopEvents.js
@@ -18,6 +18,8 @@ function TroopMeet() {
                         <a id='st_charels' target="blank" href="https://www.google.com/maps/place/3100+W+Parnell+Ave,+Milwaukee,+WI+53221/@42.9427161,-87.9567283,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ac9697935:0xe92986f1420fc0d1!8m2!3d42.9427161!4d-87.9545396?hl=en">
                             St. Charels Borromeo
                         </a></h5>
+                        <br />
+                        <h4>Please note that there will be NO MEETINGS during August, but will resume in September!</h4>
                 </div>
             </div>
         </section>
