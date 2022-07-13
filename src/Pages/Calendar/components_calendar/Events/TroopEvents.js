e3cde97dec3a2d6170f51008b59f4a8f598ed1c4
darkknight <darkknight23477@gmail.com>
2022-07-13 15:49:33 -0500

    updated dates

--- src/Pages/Calendar/components_calendar/Events/TroopEvents.js
+++ src/Pages/Calendar/components_calendar/Events/TroopEvents.js
@@ -15,12 +15,15 @@ function TroopMeet() {
                     <br />
                     <h5 className='dates'> Troop Meeting July 14th, 2022 6:30 - 8:30 PM
                         <br />
-                        <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
-                            St. Charels Borromeo
-                        </a></h5>
+                        <p className="house">Mr. Pucel's House</p></h5>
                         <br />
                         <br />
                         <h4>**Please note that there will be NO MEETINGS during August, but will resume in September!**</h4>
+                        {/* <h5 className='dates'> Troop Meeting July 14th, 2022 6:30 - 8:30 PM
+                        <br />
+                        <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
+                            St. Charels Borromeo
+                        </a></h5> */}
                 </div>
             </div>
         </section>
