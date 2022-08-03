d3c62552ed14e0395b6ef9527d034369bb9518db
darkknight <darkknight23477@gmail.com>
2022-08-03 17:42:34 -0500

    updated dates

--- src/Pages/Calendar/components_calendar/Events/SigEvents.js
+++ src/Pages/Calendar/components_calendar/Events/SigEvents.js
@@ -11,17 +11,7 @@ function SigEvent() {
             <h2>Significant Events</h2>
             <div id="event_dates">
                 <div id='significant-dates'>
-                        <h5 className='dates'> Summer Camp July 24-30th, 2022
-                        <br />
-                        <a id='st_charels' target="blank" href="https://www.google.com/maps/place/Camp+Phillips/@45.6158573,-91.8284635,17z/data=!4m5!3m4!1s0x52ac545495f14d87:0x839ff6dc0b1362b8!8m2!3d45.6158536!4d-91.8262695">
-                            Camp Philips
-                        </a></h5>
-                        <br />
-                        <h5 className='dates'> **SUMMER CAMP PACKING LIST**
-                        <br />
-                        <a id='st_charels' target="blank" href="https://urldefense.com/v3/__https://campphillips.org/download/boyScouts/CP_Packing_List.pdf__;!!PrVBqlTvcBbYrqSF!DIhk_UczeC9g2imlPa-1Wo_5NXCmLlL6p8l3y8cIBBaFDkMZGkjm9aKFBqc6w3qQW0cEPFJAk27DA1imKg$">
-                            Summer Camp Packing List
-                        </a></h5>
+                <p>No upcoming Significant events at this time.</p>
                 </div>
             </div>
         </section>
