b3154a818515dd0970c458de15ece7a5c9cec13f
darkknight <darkknight23477@gmail.com>
2022-09-22 16:54:58 -0500

    updated dates

--- src/Pages/Calendar/components_calendar/Events/SigEvents.js
+++ src/Pages/Calendar/components_calendar/Events/SigEvents.js
@@ -12,11 +12,9 @@ function SigEvent() {
             <div id="event_dates">
                 <div id='significant-dates'>
                 {/* <p>No upcoming Significant events at this time.</p> */}
-                <h5 className='dates'> SCB Picnic August 27-28th, 2022 8 PM, pickup at 6:30 AM
+                <h5 className='dates'> Palbo's Eagle Scout Project Saturday September 24th, 2022 9:30 AM
                         <br />
-                        <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
-                            St. Charels Borromeo
-                        </a></h5>
+                        <p className="house">Mr. Pucel's House</p></h5>
                 </div>
             </div>
         </section>
