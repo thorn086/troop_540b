6b15fc1da3e83c41556c950331b7d6ae03d0ae47
darkknight <darkknight23477@gmail.com>
2022-10-14 13:35:03 -0500

    updated dates

--- src/Pages/Calendar/components_calendar/Events/SigEvents.js
+++ src/Pages/Calendar/components_calendar/Events/SigEvents.js
@@ -29,12 +29,20 @@ function SigEvent() {
                         <br />
                         <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
                             St. Charels Borromeo
+                        </a>                        
+                        <br />
+                        <a id='st_charels' target="blank" href="https://www.signupgenius.com/go/20f0948acab2fa6ff2-poinsettia2">
+                            Sign Up Genius
                         </a></h5>
                         <br />
                         <h5 className='dates'> Pancake Breakfast November 6th, 2022
                         <br />
                         <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
                             St. Charels Borromeo
+                        </a>
+                        <br />
+                        <a id='st_charels' target="blank" href="https://www.signupgenius.com/go/20f0948acab2fa6ff2-pancake3">
+                            Sign Up Genius
                         </a></h5>
                         <br />
                         <h5 className='dates'> Poinsettia Pickup December 2nd, 2022
