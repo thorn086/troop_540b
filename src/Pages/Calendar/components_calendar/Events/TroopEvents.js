bc0e1e5247918719f2fefbb105957022faaa262c
darkknight <darkknight23477@gmail.com>
2022-11-03 18:00:46 -0500

    updated dates

--- src/Pages/Calendar/components_calendar/Events/TroopEvents.js
+++ src/Pages/Calendar/components_calendar/Events/TroopEvents.js
@@ -18,7 +18,7 @@ function TroopMeet() {
                             St. Charels Borromeo
                         </a></h5>
                         <br />
-                        <h5 className='dates'> Troop Meeting November 10th, 2022 6:30 - 8:30 PM
+                        <h5 className='dates'> Troop Meeting November **WREATH MONEY DUE** 10th, 2022 6:30 - 8:30 PM
                         <br />
                         <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
                             St. Charels Borromeo
