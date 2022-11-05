5c8016ffc4f9e71159ecf44567ae2f946c71ddcd
darkknight <darkknight23477@gmail.com>
2022-11-05 12:58:52 -0500

    updates dates

--- src/Pages/Calendar/components_calendar/Events/SigEvents.js
+++ src/Pages/Calendar/components_calendar/Events/SigEvents.js
@@ -21,7 +21,7 @@ function SigEvent() {
                             Sign Up Genius
                         </a></h5>
                         <br />
-                        <h5 className='dates'> Pancake Breakfast November 6th, 2022
+                        <h5 className='dates'> Pancake Breakfast November 6th, 2022 7:45-12:45 PM *Cleanup 12:45-2:30 PM*
                         <br />
                         <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
                             St. Charels Borromeo
