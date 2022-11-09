654f0395a4e5ff9d9723376cce1dd9e0dd92f4a8
darkknight <darkknight23477@gmail.com>
2022-11-09 17:09:35 -0600

    updated dates

--- src/Pages/Calendar/components_calendar/Events/SigEvents.js
+++ src/Pages/Calendar/components_calendar/Events/SigEvents.js
@@ -11,23 +11,13 @@ function SigEvent() {
             <h2>Significant Events</h2>
                 <div id='significant-dates'>
                 {/* <p>No upcoming Significant events at this time.</p> */}
-                        <h5 className='dates'> Poinsettia Sale November 5-6th, 12-13th, 2022 **ORDERS DUE NOVEMBER 14TH**
+                        <h5 className='dates'> Poinsettia Sale November 12-13th, 2022 **ORDERS DUE NOVEMBER 14TH**
                         <br />
                         <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
                             St. Charels Borromeo
                         </a>                        
                         <br />
                         <a id='st_charels' target="blank" href="https://www.signupgenius.com/go/20f0948acab2fa6ff2-poinsettia2">
-                            Sign Up Genius
-                        </a></h5>
-                        <br />
-                        <h5 className='dates'> Pancake Breakfast November 6th, 2022 7:45-12:45 PM *Cleanup 12:45-2:30 PM*
-                        <br />
-                        <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
-                            St. Charels Borromeo
-                        </a>
-                        <br />
-                        <a id='st_charels' target="blank" href="https://www.signupgenius.com/go/20f0948acab2fa6ff2-pancake3">
                             Sign Up Genius
                         </a></h5>
                         <br />
