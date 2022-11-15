3ee7c94adf888fdce67fc60a9efa256f9a95d940
darkknight <darkknight23477@gmail.com>
2022-11-15 16:56:37 -0600

    updated dates

--- src/Pages/Calendar/components_calendar/Events/PLC.js
+++ src/Pages/Calendar/components_calendar/Events/PLC.js
@@ -11,7 +11,7 @@ function PLC() {
             <h2> PLC Meetings</h2>
             <div id="plc_dates">
                 <div id='troop-dates'>
-                    <h5 className='dates'> PLC November 14th, 2022 6:00 - 6:30 PM
+                    <h5 className='dates'> PLC January 9th, 2022 6:00 - 6:30 PM
                         <br />
                         <p className="house">Mr. Pucel's House</p></h5>
 
--- src/Pages/Calendar/components_calendar/Events/TroopEvents.js
+++ src/Pages/Calendar/components_calendar/Events/TroopEvents.js
@@ -11,8 +11,15 @@ function TroopMeet() {
             <h2> Troop Meetings</h2>
                 <div id='troop-dates'>
                     <h4>**ALL scouts please BRING YOUR SCOUT BOOKS and WEAR CLASS A UNIFORM during troop meetings and outings!**</h4>
+                    <h4>**NO meetings in December! Happy Holidays!**</h4>
                         <br />
-                        <h5 className='dates'> Troop Meeting November **WREATH MONEY DUE** 10th, 2022 6:30 - 8:30 PM
+                        <h5 className='dates'> Troop Meeting January 12th, 2023 6:30 - 8:30 PM
+                        <br />
+                        <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
+                            St. Charels Borromeo
+                        </a></h5>
+                        <br />
+                        <h5 className='dates'> Troop Meeting January 26th, 2023 6:30 - 8:30 PM
                         <br />
                         <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
                             St. Charels Borromeo
