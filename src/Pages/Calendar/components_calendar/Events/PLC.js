83ac0922b7e20c1b799651271f69600831c6d817
darkknight <darkknight23477@gmail.com>
2023-01-12 16:05:40 -0600

    updated dates

--- src/Pages/Calendar/components_calendar/Events/PLC.js
+++ src/Pages/Calendar/components_calendar/Events/PLC.js
@@ -11,9 +11,10 @@ function PLC() {
             <h2> PLC Meetings</h2>
             <div id="plc_dates">
                 <div id='troop-dates'>
-                    <h5 className='dates'> PLC January 9th, 2022 6:00 - 6:30 PM
+                    No new PLC Meetings at this time
+                    {/* <h5 className='dates'> PLC January 9th, 2022 6:00 - 6:30 PM
                         <br />
-                        <p className="house">Mr. Pucel's House</p></h5>
+                        <p className="house">Mr. Pucel's House</p></h5> */}
 
                 </div>
             </div>
