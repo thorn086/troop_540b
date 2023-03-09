4ce54feb52d8c536f1220e55a418066f1750ef35
darkknight <darkknight23477@gmail.com>
2023-03-09 17:04:20 -0600

    updated dates

--- src/Pages/Calendar/components_calendar/Events/SigEvents.js
+++ src/Pages/Calendar/components_calendar/Events/SigEvents.js
@@ -10,7 +10,7 @@ function SigEvent() {
         <section id='significant__events'>
             <h2>Significant Events</h2>
                 <div id='significant-dates'>
-                        <h5 className='dates'> Scouting for Food Door Hangers March 11th
+                        <h5 className='dates'> Scouting for Food Door Hangers March 11th **Meet at 8:30AM**
                         <br />
                         <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
                             St. Charels Borromeo
