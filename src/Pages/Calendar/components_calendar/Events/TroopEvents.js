463ada13a3e3056228fe0c409e011b1a8bae0729
darkknight <darkknight23477@gmail.com>
2023-03-20 16:49:37 -0500

    updated dates

--- src/Pages/Calendar/components_calendar/Events/TroopEvents.js
+++ src/Pages/Calendar/components_calendar/Events/TroopEvents.js
@@ -12,10 +12,6 @@ function TroopMeet() {
                 <div id='troop-dates'>
                     <h4>**ALL scouts please BRING YOUR SCOUT BOOKS and WEAR CLASS A UNIFORM during troop meetings and outings!**</h4>
                         <br />
-                        <h5 className='dates'> Troop Meeting March 9th, 2023 6:30 - 8:30 PM m **CANCELLED**
-                        <br />
-                        </h5>
-                        <br />
                         <h5 className='dates'> Troop Meeting March 23rd, 2023 6:30 - 8:30 PM  **Kringle Orders Due**
                         <br />
                         <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
