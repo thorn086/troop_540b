46ea28d9f9e28080cf880e664c1e4ad0a8b8a0c9
darkknight <darkknight23477@gmail.com>
2023-04-06 17:13:03 -0500

    updated dates

--- src/Pages/Calendar/components_calendar/Events/Events.js
+++ src/Pages/Calendar/components_calendar/Events/Events.js
@@ -10,16 +10,6 @@ function Events(){
         <section id='events'>
         <h2> Troop Outings</h2>
             <div id='special-dates'>
-                        <h5 className='dates'> Pancake Breakfast April 2nd, 2023, 7:45 - 1:00 PM 
-                        <br />
-                        <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
-                            St. Charels Borromeo
-                        </a>
-                        <br />
-                        <a id='st_charels' target="blank" href="https://www.signupgenius.com/go/20f0948acab2fa6ff2-pancake4?useFullSite=true#/">
-                            Sign Up Genius
-                        </a></h5>
-                        <br />
                         <h5 className='dates'> Eagle Cave Outing April 21-23rd, 2023 
                         <br />
                         <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
