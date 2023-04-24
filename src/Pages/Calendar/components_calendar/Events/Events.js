7e169eb49f261605749ee74df3d5c21207be44a0
darkknight <darkknight23477@gmail.com>
2023-04-24 17:41:33 -0500

    updated dates

--- src/Pages/Calendar/components_calendar/Events/Events.js
+++ src/Pages/Calendar/components_calendar/Events/Events.js
@@ -10,11 +10,12 @@ function Events(){
         <section id='events'>
         <h2> Troop Outings</h2>
             <div id='special-dates'>
-                        <h5 className='dates'> Eagle Cave Outing April 21-23rd, 2023 
+                <p>No Troop Outings at this time.</p>
+                        {/* <h5 className='dates'> Eagle Cave Outing April 21-23rd, 2023 
                         <br />
                         <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
                             St. Charels Borromeo
-                        </a></h5>
+                        </a></h5> */}
             </div>
     </section>
     );
