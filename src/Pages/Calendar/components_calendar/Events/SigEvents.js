ed344777ecaf0cffbdd13e6383457ab1bbee04bc
darkknight <darkknight23477@gmail.com>
2023-05-01 17:45:36 -0500

    updated dates

--- src/Pages/Calendar/components_calendar/Events/SigEvents.js
+++ src/Pages/Calendar/components_calendar/Events/SigEvents.js
@@ -14,6 +14,20 @@ function SigEvent() {
                         <br />
                         <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
                             St. Charels Borromeo
+                        </a>
+                        <br />
+                        <a id='st_charels' target="blank" href="https://www.signupgenius.com/go/20f0948acab2fa6ff2-mothers3/64272282#/">
+                            Sign Up Genius
+                        </a></h5>
+                        <br />
+                        <h5 className='dates'> Court of Honor May 21st, 2023
+                        <br />
+                        <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
+                            St. Charels Borromeo
+                        </a>
+                        <br />
+                        <a id='st_charels' target="blank" href="https://www.signupgenius.com/go/20f0948acab2fa6ff2-spring2/64272282#/">
+                            Sign Up Genius
                         </a></h5>
                 {/* <p>No upcoming Significant events at this time.</p> */}
                 </div>
--- src/Pages/Calendar/components_calendar/Events/TroopEvents.js
+++ src/Pages/Calendar/components_calendar/Events/TroopEvents.js
@@ -12,7 +12,13 @@ function TroopMeet() {
                 <div id='troop-dates'>
                     <h4>**ALL scouts please BRING YOUR SCOUT BOOKS and WEAR CLASS A UNIFORM during troop meetings and outings!**</h4>
                         <br />
-                        <h5 className='dates'> Troop Meeting April 27th, 2023 6:30 - 8:30 PM
+                        <h5 className='dates'> Troop Meeting May 11th, 2023 6:30 - 8:30 PM
+                        <br />
+                        <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
+                            St. Charels Borromeo
+                        </a></h5>
+                        <br />
+                        <h5 className='dates'> Troop Meeting May 25th, 2023 6:30 - 8:30 PM
                         <br />
                         <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
                             St. Charels Borromeo
