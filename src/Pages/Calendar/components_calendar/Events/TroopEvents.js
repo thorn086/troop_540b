32c7689bc4bc814051d9d9ce006cf59741657a7d
darkknight <darkknight23477@gmail.com>
2023-06-26 16:55:50 -0500

    updated dates

--- src/Pages/Calendar/components_calendar/Events/TroopEvents.js
+++ src/Pages/Calendar/components_calendar/Events/TroopEvents.js
@@ -14,9 +14,13 @@ function TroopMeet() {
                         <br />
                         <h5 className='dates'> Summer Camp Meeting July 16th, 2023 1-2:00 PM
                         <br />
+                        <p className="house">Mr. Pucel's House</p></h5>
+                        {/* <br />
+                        <h5 className='dates'> Summer Camp Meeting July 16th, 2023 1-2:00 PM
+                        <br />
                         <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
                             St. Charels Borromeo
-                        </a></h5>
+                        </a></h5> */}
                 </div>
         </section>
     );
