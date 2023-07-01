1491c91ee86b42db51c0679e56d302eb375a6a3b
darkknight <darkknight23477@gmail.com>
2023-07-01 18:57:48 -0500

    updated dates

--- src/Pages/Calendar/components_calendar/Events/SigEvents.js
+++ src/Pages/Calendar/components_calendar/Events/SigEvents.js
@@ -1,16 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import "./Events.css";
 
 function SigEvent() {
-  const linkStyle = {
-    color: "white",
-    padding: "6px",
-  };
+  // const linkStyle = {
+  //   color: "white",
+  //   padding: "6px",
+  // };
   return (
     <section id="significant__events">
       <h2>Significant Events</h2>
       <div id="significant-dates">
+      <h5 className='dates'> 4th of July Parade, 2023 8:30 AM
+                        <br />
+                        <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
+                            St. Charels Borromeo
+                        </a></h5>   
+                        <br />
       <h5 className='dates'> Summer Camp July 23-29th, 2023
                         <br />
                         <a id='st_charels' target="blank" href="https://www.google.com/maps/dir/42.9522944,-87.9788032/camp+phillips+wi/@44.262081,-92.0088925,7z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x52ac545495f14d87:0x839ff6dc0b1362b8!2m2!1d-91.8262695!2d45.6158536?entry=ttu">
