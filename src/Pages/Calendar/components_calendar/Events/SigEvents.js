a510bbbe14e39763a480cfc27a365747bbf3292a
Darren Rose <thorn086@gmail.com>
2023-08-21 22:50:12 -0500

    chore: update church event

--- src/Pages/Calendar/components_calendar/Events/SigEvents.js
+++ src/Pages/Calendar/components_calendar/Events/SigEvents.js
@@ -3,15 +3,25 @@ import React from "react";
 import "./Events.css";
 
 function SigEvent() {
-  // const linkStyle = {
-  //   color: "white",
-  //   padding: "6px",
-  // };
   return (
     <section id="significant__events">
       <h2>Significant Events</h2>
       <div id="significant-dates">
-        <p>No upcoming Significant events at this time.</p>
+        <p>
+          Scouts watch the Church grounds <br />
+          <br />
+          9:30 PM to 7:00 AM <br />
+          <br />
+          August 26th to August 27th, 2023
+          <br />
+          <a
+            id="st_charels"
+            target="blank"
+            href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366"
+          >
+            St. Charels Borromeo
+          </a>
+        </p>
       </div>
     </section>
   );
