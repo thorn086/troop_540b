f106e9499756762b672b51dc45f79de41937287a
darkknight <darkknight23477@gmail.com>
2023-08-28 13:26:07 -0500

    updated dates

--- src/Pages/Calendar/components_calendar/Events/SigEvents.js
+++ src/Pages/Calendar/components_calendar/Events/SigEvents.js
@@ -7,21 +7,7 @@ function SigEvent() {
     <section id="significant__events">
       <h2>Significant Events</h2>
       <div id="significant-dates">
-        <p>
-          Scouts watch the Church grounds <br />
-          <br />
-          9:30 pm to 7:00 am <br />
-          <br />
-          August 26th to August 27th, 2023
-          <br />
-          <a
-            id="st_charels"
-            target="blank"
-            href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366"
-          >
-            St. Charels Borromeo
-          </a>
-        </p>
+      <p>No Significant Events at this time.</p>
       </div>
     </section>
   );
--- src/Pages/Calendar/components_calendar/Events/TroopEvents.js
+++ src/Pages/Calendar/components_calendar/Events/TroopEvents.js
@@ -12,13 +12,17 @@ function TroopMeet() {
                 <div id='troop-dates'>
                     <h4>**ALL scouts please BRING YOUR SCOUT BOOKS and WEAR CLASS A UNIFORM during troop meetings and outings!**</h4>
                         <br />
-                        <p>**NO meetings in August.** See you all in September!</p>
-                        {/* <br />
-                        <h5 className='dates'> Summer Camp Meeting July 16th, 2023 1-2:00 PM
+                        <h5 className='dates'> Troop Meeting September 14th, 2023 6:30-8 PM
                         <br />
                         <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
                             St. Charels Borromeo
-                        </a></h5> */}
+                        </a></h5>
+                        <br />
+                        <h5 className='dates'> Troop Meeting September 28th, 2023 6:30-8 PM
+                        <br />
+                        <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
+                            St. Charels Borromeo
+                        </a></h5>
                 </div>
         </section>
     );
