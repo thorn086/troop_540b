327eea773d6289f9a6e0188f86d270aadc079510
darkknight <darkknight23477@gmail.com>
2023-09-03 11:32:42 -0500

    added sign up genius

--- src/Pages/Calendar/components_calendar/Events/Events.js
+++ src/Pages/Calendar/components_calendar/Events/Events.js
@@ -16,6 +16,9 @@ function Events(){
                         <br />
                         <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
                             St. Charels Borromeo
+                        </a>
+                        <a id='st_charels' target="blank" href="https://www.signupgenius.com/go/20F0948ACAB2FA6FF2-aluminum1">
+                            Sign Up Genius
                         </a></h5>
             </div>
     </section>
