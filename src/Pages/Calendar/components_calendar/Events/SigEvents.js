187de94f9b36c4e2a7f9b93e070e21af720ad30f
darkknight <darkknight23477@gmail.com>
2023-09-16 11:46:53 -0500

    updated dates

--- src/Pages/Calendar/components_calendar/Events/SigEvents.js
+++ src/Pages/Calendar/components_calendar/Events/SigEvents.js
@@ -7,7 +7,24 @@ function SigEvent() {
     <section id="significant__events">
       <h2>Significant Events</h2>
       <div id="significant-dates">
-      <p>No Significant Events at this time.</p>
+                        <h5 className='dates'> Open Historic Milwaukee September 23-24th.
+                        <br />
+                        <a id='st_charels' target="blank" href="https://historicmilwaukee.org/doors-open/buildings/">
+                            Historic Milwaukee
+                        </a></h5>
+                        <br />
+                        <h5 className='dates'> Canoeing at the Urban Ecology Center, October 7th, 10-2 PM, Meet at 9:45
+                        <br />
+                        <a id='st_charels' target="blank" href="https://calendar.urbanecologycenter.org/event/take-it-outside-day-canoeing/">
+                            Urban Ecology Center
+                        </a></h5>
+                        <br />
+                        <h5 className='dates'> Urban Ecology Center Climbing, November 3rd, 4-6 PM
+                        <br />
+                        <a id='st_charels' target="blank" href="https://calendar.urbanecologycenter.org/event/on-belay-outdoor-rock-climbing-9/">
+                            Urban Ecology Center
+                        </a></h5>
+      {/* <p>No Significant Events at this time.</p> */}
       </div>
     </section>
   );
