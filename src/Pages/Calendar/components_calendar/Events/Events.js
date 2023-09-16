de1bdf36c4da664c1d85f3131b9da74894145dcc
darkknight <darkknight23477@gmail.com>
2023-09-16 11:50:15 -0500

    updated dates

--- src/Pages/Calendar/components_calendar/Events/Events.js
+++ src/Pages/Calendar/components_calendar/Events/Events.js
@@ -10,8 +10,25 @@ function Events(){
         <section id='events'>
         <h2> Troop Outings</h2>
             <div id='special-dates'>
+            <h5 className='dates'> Open Historic Milwaukee September 23-24th.
+            <br />
+            <a id='st_charels' target="blank" href="https://historicmilwaukee.org/doors-open/buildings/">
+                Historic Milwaukee
+            </a></h5>
+            <br />
+            <h5 className='dates'> Canoeing at the Urban Ecology Center, October 7th, 10-2 PM, Meet at 9:45
+            <br />
+            <a id='st_charels' target="blank" href="https://calendar.urbanecologycenter.org/event/take-it-outside-day-canoeing/">
+                Urban Ecology Center
+            </a></h5>
+            <br />
+            <h5 className='dates'> Urban Ecology Center Climbing, November 3rd, 4-6 PM
+            <br />
+            <a id='st_charels' target="blank" href="https://calendar.urbanecologycenter.org/event/on-belay-outdoor-rock-climbing-9/">
+                Urban Ecology Center
+            </a></h5>
             {/* <h4>**Summer Camp MEDICAL FORMS DUE July 1st!!!**</h4> */}
-                <p>No Troop Outings at this time.</p>
+                {/* <p>No Troop Outings at this time.</p> */}
             </div>
     </section>
     );
--- src/Pages/Calendar/components_calendar/Events/SigEvents.js
+++ src/Pages/Calendar/components_calendar/Events/SigEvents.js
@@ -7,24 +7,7 @@ function SigEvent() {
     <section id="significant__events">
       <h2>Significant Events</h2>
       <div id="significant-dates">
-                        <h5 className='dates'> Open Historic Milwaukee September 23-24th.
-                        <br />
-                        <a id='st_charels' target="blank" href="https://historicmilwaukee.org/doors-open/buildings/">
-                            Historic Milwaukee
-                        </a></h5>
-                        <br />
-                        <h5 className='dates'> Canoeing at the Urban Ecology Center, October 7th, 10-2 PM, Meet at 9:45
-                        <br />
-                        <a id='st_charels' target="blank" href="https://calendar.urbanecologycenter.org/event/take-it-outside-day-canoeing/">
-                            Urban Ecology Center
-                        </a></h5>
-                        <br />
-                        <h5 className='dates'> Urban Ecology Center Climbing, November 3rd, 4-6 PM
-                        <br />
-                        <a id='st_charels' target="blank" href="https://calendar.urbanecologycenter.org/event/on-belay-outdoor-rock-climbing-9/">
-                            Urban Ecology Center
-                        </a></h5>
-      {/* <p>No Significant Events at this time.</p> */}
+      <p>No Significant Events at this time.</p>
       </div>
     </section>
   );
