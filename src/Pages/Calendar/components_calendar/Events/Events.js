f1cd8ddff251d9a5c37322e8abf2436ceb2a3b21
darkknight <darkknight23477@gmail.com>
2023-10-08 18:26:09 -0500

    updated dates

--- src/Pages/Calendar/components_calendar/Events/Events.js
+++ src/Pages/Calendar/components_calendar/Events/Events.js
@@ -10,12 +10,6 @@ function Events(){
         <section id='events'>
         <h2> Troop Outings</h2>
             <div id='special-dates'>
-            <h5 className='dates'> Canoeing at the Urban Ecology Center, October 7th, 10-2 PM, Meet at 9:45
-            <br />
-            <a id='st_charels' target="blank" href="https://calendar.urbanecologycenter.org/event/take-it-outside-day-canoeing/">
-                Urban Ecology Center
-            </a></h5>
-            <br />
             <h5 className='dates'> Urban Ecology Center Climbing, November 3rd, 4-6 PM
             <br />
             <a id='st_charels' target="blank" href="https://calendar.urbanecologycenter.org/event/on-belay-outdoor-rock-climbing-9/">
