b6e56e6f283feba922e626b65e653671fe499b0f
darkknight <darkknight23477@gmail.com>
2023-10-10 18:05:58 -0500

    updated dates

--- src/Pages/Calendar/components_calendar/Events/SigEvents.js
+++ src/Pages/Calendar/components_calendar/Events/SigEvents.js
@@ -7,7 +7,26 @@ function SigEvent() {
     <section id="significant__events">
       <h2>Significant Events</h2>
       <div id="significant-dates">
-      <p>No Significant Events at this time.</p>
+      {/* <p>No Significant Events at this time.</p> */}
+      <br />
+      <h5 className='dates'> Pancake Breakfast October 22nd, 2023 7:45-1 PM
+      <br />
+      <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
+      St. Charels Borromeo
+      </a>
+      <a id='st_charels' target="blank" href="https://www.signupgenius.com/go/20F0948ACAB2FA6FF2-45020973-pancake">
+       Sign Up Genius
+      </a></h5>
+      <br />
+      <h5 className='dates'> Court of Honor October 29th, 2023 5-7:30 PM
+      <br />
+      <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
+      St. Charels Borromeo
+      </a>
+      <a id='st_charels' target="blank" href="https://www.signupgenius.com/go/20F0948ACAB2FA6FF2-45021154-fall/64272282#/">
+       Sign Up Genius
+      </a></h5>
+                      
       </div>
     </section>
   );
