87b6b1176c02086e47cdc4bf925f79817a353e00
darkknight <darkknight23477@gmail.com>
2023-10-23 17:44:55 -0500

    updated dates

--- src/Pages/Calendar/components_calendar/Events/PLC.js
+++ src/Pages/Calendar/components_calendar/Events/PLC.js
@@ -11,7 +11,7 @@ function PLC() {
             <h2> PLC Meetings</h2>
             <div id="plc_dates">
                 <div id='troop-dates'>
-                    <h5 className='dates'> PLC October 9th, 2022 6:00 - 6:30 PM
+                    <h5 className='dates'> PLC December 11th, 2022 6:00 - 6:30 PM
                         <br />
                         <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
                             St. Charels Borromeo
--- src/Pages/Calendar/components_calendar/Events/TroopEvents.js
+++ src/Pages/Calendar/components_calendar/Events/TroopEvents.js
@@ -14,12 +14,6 @@ function TroopMeet() {
                     <br />
                     <h4>**WREATH ORDER FORMS DUE OCT 21ST!!!**</h4>
                         <br />
-                        <h5 className='dates'> Troop Meeting October 12th, 2023 6:30-8 PM
-                        <br />
-                        <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
-                            St. Charels Borromeo
-                        </a></h5>
-                        <br />
                         <h5 className='dates'> Troop Meeting October 26th, 2023 6:30-8 PM
                         <br />
                         <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
