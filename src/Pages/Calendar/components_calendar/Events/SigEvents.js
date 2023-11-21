3d6195903c548ac61f4734fc17d44f6f2be9c40e
darkknight <darkknight23477@gmail.com>
2023-11-21 17:34:14 -0600

    updated dates

--- src/Pages/Calendar/components_calendar/Events/SigEvents.js
+++ src/Pages/Calendar/components_calendar/Events/SigEvents.js
@@ -7,24 +7,7 @@ function SigEvent() {
     <section id="significant__events">
       <h2>Significant Events</h2>
       <div id="significant-dates">
-      <h5 className='dates'> Poinsettia Sale November 11-12th,
-      <br />
-      <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
-       St. Charels Borromeo
-      </a>
-      <a id='st_charels' target="blank" href="https://www.signupgenius.com/go/20F0948ACAB2FA6FF2-45944130-poinsettia/64272282#/">
-       Sign Up Genius
-      </a></h5>
-      <br/>
-      <h5 className='dates'> Poinsettia Sale November 18-19th,
-      <br />
-      <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
-       St. Charels Borromeo
-      </a>
-      <a id='st_charels' target="blank" href="https://www.signupgenius.com/go/20F0948ACAB2FA6FF2-45944130-poinsettia/64272282#/">
-       Sign Up Genius
-      </a></h5>
-      {/* <p>No Significant Events at this time.</p> */}
+      <p>No Significant Events at this time.</p>
                       
       </div>
     </section>
