d7da709da4ea4d3ed541ae7513f90c20648799c0
darkknight <darkknight23477@gmail.com>
2024-02-07 17:43:22 -0600

    updated dates

--- src/Pages/Calendar/components_calendar/Events/PLC.js
+++ src/Pages/Calendar/components_calendar/Events/PLC.js
@@ -11,7 +11,7 @@ function PLC() {
             <h2> PLC Meetings</h2>
             <div id="plc_dates">
                 <div id='troop-dates'>
-                    <h5 className='dates'> PLC December 11th, 2022 6:00 - 6:30 PM
+                    <h5 className='dates'> PLC February 12th, 2022 6:00 - 6:30 PM
                         <br />
                         <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
                             St. Charels Borromeo
--- src/Pages/Calendar/components_calendar/Events/SigEvents.js
+++ src/Pages/Calendar/components_calendar/Events/SigEvents.js
@@ -7,12 +7,7 @@ function SigEvent() {
     <section id="significant__events">
       <h2>Significant Events</h2>
       <div id="significant-dates">
-                        <h5 className='dates'> Klondike January 27th 2024
-                        <br />
-                        <a id='st_charels' target="blank" href="https://www.google.com/maps/dir/42.9333629,-87.9628829/oh+da+kot+a/@42.7498886,-88.4498389,10z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x880586721cd4eec5:0x9ba5ce69d6712d0b!2m2!1d-88.299794!2d42.6076?entry=ttu">
-                            Camp Oh-Da-Ko-Ta
-                        </a></h5>
-      {/* <p>No Significant Events at this time.</p> */}
+      <p>No Significant Events at this time.</p>
                       
       </div>
     </section>
