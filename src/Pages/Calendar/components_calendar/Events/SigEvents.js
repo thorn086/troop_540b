64485fe4983c02bf396f4809e46c43d6db58d5ab
darkknight <darkknight23477@gmail.com>
2024-02-29 20:51:30 -0600

    updated dates

--- src/Pages/Calendar/components_calendar/Events/SigEvents.js
+++ src/Pages/Calendar/components_calendar/Events/SigEvents.js
@@ -7,7 +7,18 @@ function SigEvent() {
     <section id="significant__events">
       <h2>Significant Events</h2>
       <div id="significant-dates">
-      <p>No Significant Events at this time.</p>
+      <h5 className='dates'> Scouting For Food Flyer Dropoff, April 9th 8:30 AM
+                        <br />
+                        <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
+                            St. Charels Borromeo
+                        </a></h5>
+                        <br/>
+                        <h5 className='dates'> Scouting For Food Pickup, April 16th 8:30 AM
+                        <br />
+                        <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
+                            St. Charels Borromeo
+                        </a></h5>
+      {/* <p>No Significant Events at this time.</p> */}
                       
       </div>
     </section>
