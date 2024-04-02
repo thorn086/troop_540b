f9619d7938ca4b3f4b104932f6083cbc3bb3011b
darkknight <darkknight23477@gmail.com>
2024-04-01 19:02:17 -0500

    updated dates

--- src/Pages/Calendar/components_calendar/Events/SigEvents.js
+++ src/Pages/Calendar/components_calendar/Events/SigEvents.js
@@ -7,30 +7,7 @@ function SigEvent() {
     <section id="significant__events">
       <h2>Significant Events</h2>
       <div id="significant-dates">
-      <h5 className='dates'> Scouting For Food Flyer Dropoff, March 9th 8:30 AM
-                        <br />
-                        <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
-                            St. Charels Borromeo
-                        </a></h5>
-                        <br/>
-                        <h5 className='dates'> Scouting For Food Pickup, March 16th 8:30 AM
-                        <br />
-                        <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
-                            St. Charels Borromeo
-                        </a></h5>
-                        <br/>
-                        <h5 className='dates'> Easter Bunny Pancake Breakfast, March 24th 7:45 AM - 12 PM
-                        <br />
-                        <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
-                            St. Charels Borromeo
-                        </a></h5>
-                        <br/>
-                        <h5 className='dates'> Easter Vigil, March 30th 7-8 PM
-                        <br />
-                        <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
-                            St. Charels Borromeo
-                        </a></h5>
-      {/* <p>No Significant Events at this time.</p> */}
+      <p>No Significant Events at this time.</p>
                       
       </div>
     </section>
