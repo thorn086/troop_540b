92b8b5837add6d9427d87cae50a8f0a48f81e0d0
darkknight <darkknight23477@gmail.com>
2024-04-08 18:55:56 -0500

    updated dates

--- src/Pages/Calendar/components_calendar/Events/Events.js
+++ src/Pages/Calendar/components_calendar/Events/Events.js
@@ -11,7 +11,18 @@ function Events(){
         <h2> Troop Outings</h2>
             <div id='special-dates'>
             {/* <h4>**Summer Camp MEDICAL FORMS DUE July 1st!!!**</h4> */}
-                <p>No Troop Outings at this time.</p>
+                {/* <p>No Troop Outings at this time.</p> */}
+                <h5 className='dates'> Mother's Day Flower Sale May 11th, 2024 2-5:30 PM
+                        <br />
+                        <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
+                            St. Charels Borromeo
+                        </a></h5>
+                        <br />
+                        <h5 className='dates'> Mother's Day Flower Sale May 12th, 2024 7:30 -12:30 PM
+                        <br />
+                        <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
+                            St. Charels Borromeo
+                        </a></h5>
             </div>
     </section>
     );
