ef13d602d2efc91493848f8443049f63ca9d55a2
darkknight <darkknight23477@gmail.com>
2024-06-06 10:56:49 -0500

    updated dates

--- src/Pages/Calendar/components_calendar/Events/SigEvents.js
+++ src/Pages/Calendar/components_calendar/Events/SigEvents.js
@@ -7,12 +7,7 @@ function SigEvent() {
     <section id="significant__events">
       <h2>Significant Events</h2>
       <div id="significant-dates">
-      {/* <p>No Significant Events at this time.</p> */}
-      <h5 className='dates'> Court of Honor May 19th, 2024 5-7:30 PM
-     <br />
-     <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
-      St. Charels Borromeo
-     </a></h5>
+      <p>No Significant Events at this time.</p>
       </div>
     </section>
   );
--- src/Pages/Calendar/components_calendar/Events/TroopEvents.js
+++ src/Pages/Calendar/components_calendar/Events/TroopEvents.js
@@ -12,13 +12,13 @@ function TroopMeet() {
                 <div id='troop-dates'>
                     <h4>**ALL scouts please BRING YOUR SCOUT BOOKS and WEAR CLASS A UNIFORM during troop meetings and outings!**</h4>
                         <br />
-                        <h5 className='dates'> Troop Meeting May 9th, 2024 6:30-8 PM
+                        <h5 className='dates'> Troop Meeting June 13th, 2024 6:30-8 PM
                         <br />
                         <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
                             St. Charels Borromeo
                         </a></h5>
                         <br />
-                        <h5 className='dates'> Troop Meeting May 23rd, 2024 6:30-8 PM
+                        <h5 className='dates'> Troop Meeting June 27th, 2024 6:30-8 PM
                         <br />
                         <a id='st_charels' target="blank" href="https://www.google.com/maps/place/St.+Charles+Borromeo+School/@42.942615,-87.9567306,17z/data=!3m1!4b1!4m5!3m4!1s0x8805116ad48ca715:0x4cdeaa0ccbf337!8m2!3d42.9426111!4d-87.9545366">
                             St. Charels Borromeo
